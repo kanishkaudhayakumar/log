774a329ae1a54939b4583bcbdfb585bcc7d0e9f0
kanishudhya <kanishudhya@gmail.com>
2020-10-12 05:05:51 -0700

    first commit1

---
+++ igloo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class igloo{
+	public static void main(String args[])
+	{
+		System.out.println("welcome all");
+		System.out.println("hello world");
+		System.out.println("hii all");
+		System.out.println("welcome to github");
+	}
+}
+
+
