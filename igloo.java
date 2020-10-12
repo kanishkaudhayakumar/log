ed35be13e89d37ab69cd1ca5f5dc153d87d70bab
kanishudhya <kanishudhya@gmail.com>
2020-10-12 05:11:46 -0700

    second commit2

--- igloo.java
+++ igloo.java
@@ -4,9 +4,9 @@ public class igloo{
 	{
 		System.out.println("welcome all");
 		System.out.println("hello world");
-		System.out.println("hii all");
+		System.out.println("helllooooo welcome");
 		System.out.println("welcome to github");
-	}
+ System.out.println("welcome to git");	}
 }
 
 
